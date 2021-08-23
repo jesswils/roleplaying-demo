36f20ff1087611ec4db46497c7597dd3da5e80b3
jesswils <jessicalaurenwilson1@gmail.com>
2021-08-23 14:06:07 +0100

    amended margins for text transition

--- lib/components/text-box.dart
+++ lib/components/text-box.dart
@@ -1,1 +1,26 @@
 import 'package:flutter/material.dart';
+
+class TextTransition extends StatefulWidget {
+  const TextTransition({Key? key}) : super(key: key);
+
+  @override
+  _TextTransitionState createState() => _TextTransitionState();
+}
+
+class _TextTransitionState extends State<TextTransition> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(9.0, 25.0, 9.0, 0.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
